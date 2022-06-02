778b3635cb2232dc624ce698501861a392b355f8
YsabelF <82843022+YsabelF@users.noreply.github.com>
2022-06-02 18:57:28 -0500

    Actualización de ProductoRepository

--- src/main/java/com/happypets/repository/ProductoRepository.java
+++ src/main/java/com/happypets/repository/ProductoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.happypets.entity.Producto;
@@ -11,7 +12,7 @@ import com.happypets.entity.Producto;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer>{
 
-	@Query("select p from Producto p where desc_producto like %:nombre%")
-	public List<Producto> buscarProductoProNombre(String nombre);
-
+	@Query("select p from Producto p where desc_producto like :nombre")
+	public abstract List<Producto> buscarProductoProNombre(@Param("nombre")String nombre);
+	
 }
