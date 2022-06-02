540775742c1faa54420f54d930836616abace000
YsabelF <82843022+YsabelF@users.noreply.github.com>
2022-06-02 18:58:00 -0500

    Actualizacion ProductoService

--- src/main/java/com/happypets/service/ProductoService.java
+++ src/main/java/com/happypets/service/ProductoService.java
@@ -6,9 +6,11 @@ import com.happypets.entity.Producto;
 
 public interface ProductoService {
 
-	public abstract Producto insertaActualizaProducto(Producto producto);
-	
+	public abstract Producto insertaProducto(Producto producto);
+	public abstract Producto actualizaProducto(Producto producto);
 	public abstract List<Producto> listaProducto();
+	
+	public abstract Producto obtenerProducto(int id);
 	
 	public abstract List<Producto> listaProductoPorNombre(String nombre);
 	
