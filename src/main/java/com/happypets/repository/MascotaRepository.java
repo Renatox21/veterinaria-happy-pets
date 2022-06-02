ba74f9d4408f927f6b37faeba688065b008c1ad9
SesarinaVasquez <106773753+SesarinaVasquez@users.noreply.github.com>
2022-06-02 18:33:07 -0500

    Add files via upload

---
+++ src/main/java/com/happypets/repository/MascotaRepository.java
@@ -0,0 +1,11 @@
+package com.happypets.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.happypets.entity.Mascota;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<Mascota, Integer>{
+
+}
