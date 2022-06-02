21b8b4eaa204ac8f2cb7a5609aebd01d3b11b879
YsabelF <82843022+YsabelF@users.noreply.github.com>
2022-06-02 10:29:24 -0500

    Add files via upload
    Actualizacion ProductoRepository

--- src/main/java/com/happypets/repository/ProductoRepository.java
+++ src/main/java/com/happypets/repository/ProductoRepository.java
@@ -1,6 +1,9 @@
 package com.happypets.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.happypets.entity.Producto;
