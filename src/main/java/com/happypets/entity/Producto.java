340ac0952466def5ddd8811639067a9ff4713037
Renatox21 <82481366+Renatox21@users.noreply.github.com>
2022-06-01 21:32:03 -0500

    Add files via upload
    ProductoEntity

---
+++ src/main/java/com/happypets/entity/Producto.java
@@ -0,0 +1,29 @@
+package com.happypets.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_producto")
+public class Producto {
+
+	@Id	
+	private id id_producto
+	
+	private String desc_product
+	
+	private int stock
+	
+	private double pre_uni
+	
+	private int id_categoria	
+	
+}
