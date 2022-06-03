138750283ce2be9d0b6463732d05815bc6346d61
Bratt Vallejo <92759579+brattvell@users.noreply.github.com>
2022-06-02 19:25:57 -0500

    Add files via upload

--- src/main/java/com/happypets/controller/MascotaController.java
+++ src/main/java/com/happypets/controller/MascotaController.java
@@ -1,58 +1,36 @@
 package com.happypets.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.happypets.entity.Mascota;
-import com.happypets.entity.Usuario;
 import com.happypets.service.MascotaService;
 import com.happypets.util.Constantes;
 
 @RestController
 @RequestMapping("rest/mascota")
 public class MascotaController {
-
+	
 	@Autowired
 	private MascotaService service;
 	
-	//http://localhost:8090/rest/mascota/listarMascota
-	@GetMapping("/listarMascota")
-	public List<Mascota> obtenerMascotas(){			
-		return service.listarMascotas();
-	}
-	
-	
-	//request
-	/*El formato JSON es el siguiente:
-	* 
-	  {
-       "id_mascota": 2,
-       "nombre_mascota": "Kazzu",
-       "tipo_mascota": "Perro",
-       "raza_mascota": "Golden",
-       "id_vacunas": 1,
-       "fec_mascota": "2022-01-02",
-       "fec_nac": "2021-05-03"
-      }
-	* */
-	//http://localhost:8090/rest/mascota/actualizarMascota	
-	@PutMapping("/actualizarMascota")
-	public ResponseEntity<Map<String, Object>> actualizarMascota(@RequestBody Mascota mascota){
+	@PostMapping("/agregarMascota")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> insertaMascota(@RequestBody Mascota masc){
 		
 		Map<String, Object> salida = new HashMap<String, Object>();
 		
 		try {
-			
-			Mascota objMascota = service.actualizarMascota(mascota);
+						
+			Mascota objMascota = service.insertaActualizaMascota(masc);
 			if(objMascota == null){
 				salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
 			}else {
@@ -65,5 +43,5 @@ public class MascotaController {
 		}
 		return ResponseEntity.ok(salida);
 	}
-	
+
 }
