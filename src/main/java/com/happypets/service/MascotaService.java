6622be6ef791069dc2128f023b7eabd73aefdc58
Ghian987 <90944439+Ghian987@users.noreply.github.com>
2022-06-02 20:05:21 -0500

    servicemascota

--- src/main/java/com/happypets/service/MascotaService.java
+++ src/main/java/com/happypets/service/MascotaService.java
@@ -1,9 +1,10 @@
 package com.happypets.service;
 
+import java.util.List;
+
 import com.happypets.entity.Mascota;
 
 public interface MascotaService {
-	
-	public abstract Mascota insertaActualizaMascota(Mascota mascota);
-	
+
+	public abstract List<Mascota> listaMascotaPorNombre(String nombre);
 }
