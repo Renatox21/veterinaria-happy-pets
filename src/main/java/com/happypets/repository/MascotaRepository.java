14275aa9b5d6bb704d79f43c4c0aeb26f316766e
Ghian987 <90944439+Ghian987@users.noreply.github.com>
2022-06-02 20:04:53 -0500

    repositorymascota

--- src/main/java/com/happypets/repository/MascotaRepository.java
+++ src/main/java/com/happypets/repository/MascotaRepository.java
@@ -1,11 +1,16 @@
 package com.happypets.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.happypets.entity.Mascota;
 
 @Repository
 public interface MascotaRepository extends JpaRepository<Mascota, Integer>{
-	
+
+	@Query("select m from Mascota m where nombre_mascota like %:nombre%")
+	public List<Mascota> buscarMascotaPorNombre(String nombre);
 }
