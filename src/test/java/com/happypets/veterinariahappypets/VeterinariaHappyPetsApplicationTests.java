1fe5d079bb1c1d156a7d85ea2bb3ca2431d7ae8b
Renatox21 <82481366+Renatox21@users.noreply.github.com>
2022-06-28 22:32:34 -0500

    Implementacion selenium

--- src/test/java/com/happypets/veterinariahappypets/VeterinariaHappyPetsApplicationTests.java
+++ src/test/java/com/happypets/veterinariahappypets/VeterinariaHappyPetsApplicationTests.java
@@ -1,13 +1,34 @@
 package com.happypets.veterinariahappypets;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VeterinariaHappyPetsApplicationTests {
 
+	private WebDriver driver;
+	
 	@Test
 	void contextLoads() {
+		
+		System.setProperty("webdriver.chrome.driver","src/test/chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+	
+		driver.get("http://localhost:4200/crudProducto");
+
+		driver.findElement(By.id("id_mostrar_registro")).click();
+				
+		driver.findElement(By.id("descripcion")).sendKeys("Galleta para gato");
+		driver.findElement(By.id("stock")).sendKeys("120");
+		driver.findElement(By.id("preUni")).sendKeys("30.00");
+		driver.findElement(By.id("idCategoria")).sendKeys("1");	
+		
+		driver.findElement(By.id("id_reg_registra")).click();
+		
+		
 	}
 
 }
