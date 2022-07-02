eb66309b3bb5dbef7080d229d20816ded9167888
Bratt Vallejo <92759579+brattvell@users.noreply.github.com>
2022-07-02 17:12:30 -0500

    Add files via upload

---
+++ src/main/java/com/happypets/DTO/ComprobanteDTO.java
@@ -0,0 +1,23 @@
+package com.happypets.DTO;
+
+public class ComprobanteDTO {
+	
+	private String num;
+	
+	private String tipo_comprobante;
+	
+	private String ruc;
+	
+	private String dni;
+	
+	private int id_cliente;
+	
+	private int id_empleado;
+	
+	private int cantidad;
+	
+	private double pre_uni;
+
+	private double descuento;
+
+}
